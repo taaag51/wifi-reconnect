9d27b238abd82475304bfa7af5e9760d90798bb5
taaag51 <inat0510@gmail.com>
2024-10-06 11:26:43 +0900

    fix:pingは3秒だけ待つようにする

--- wifi_reconnect.sh
+++ wifi_reconnect.sh
@@ -13,7 +13,7 @@ log_message() {
 log_message "Wi-Fi監視スクリプトを開始しました。"
 
 while true; do
-    if ! ping -c 1 google.com &>/dev/null; then
+    if ! ping -c 1 -W 3 google.com &>/dev/null; then
         log_message "インターネット接続が切断されました。Wi-Fiを再接続します。"
         networksetup -setairportpower en0 off
         sleep 5
